35c7c70bb29baf31b5a2a9156c5a4ec0c099b0db
lomelipablo1 <lomelipablo1@gmail.com>
2018-01-18 12:50:14 -0700

    Portfolio Website is now public.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+"use strict";
+
+function hovermm(element) {
+element.setAttribute('src', 'images/mmgif.gif' );
+}
+
+function unhovermm(element) {
+	element.setAttribute('src', 'images/mmpng.png');
+}
+
+function hovermirage(element) {
+element.setAttribute('src', 'images/miragegif.gif' );
+}
+
+function unhovermirage(element) {
+	element.setAttribute('src', 'images/miragepng.png');
+}
+
